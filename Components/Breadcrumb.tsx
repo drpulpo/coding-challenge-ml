d7250bcc4d40ba0c6667e9df4af207588999124a
David Nuñez <david.nunez@techmex.mx>
2022-11-19 21:52:41 -0600

    Search Server Side Rendered

---
+++ Components/Breadcrumb.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Breadcrumb.module.css';
+
+export default function Breadcrumb(){
+
+    return(
+            <div>
+                <p>Breadcrumb > Breadcrumb > Breadcrumb</p>
+            </div>
+
+
+    )
+}
--- pages/items/index.tsx
+++ pages/items/index.tsx
@@ -1,8 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
+import CajaBusqueda from '../../Components/CajaBusqueda'
 
-export default function ItemList() {
+
+// This gets called on every request
+export async function getServerSideProps(context) {
+    // Fetch data from external API
+    console.log(context)
+    const res = await fetch(`http://localhost:3000/api/items?q=:${context.query.search}`)
+    const data = await res.json()
+    const items = data.results.slice(0,4)
+
+    // Pass data to the page via props
+    return { props: { items } }
+}
+
+export default function ItemListPage({ items }) {
     return (
         <div className={styles.container}>
             <Head>
@@ -11,10 +25,13 @@ export default function ItemList() {
             <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className={styles.main}>
-            <p className={styles.description}>
-                Lista Items
-            </p>
+            <main>
+                <CajaBusqueda/>
+                <ul>
+                    {items.map(item => (
+                            <li>{item.title}</li>
+                    ))}
+                </ul>
             </main>
         </div>
     )
